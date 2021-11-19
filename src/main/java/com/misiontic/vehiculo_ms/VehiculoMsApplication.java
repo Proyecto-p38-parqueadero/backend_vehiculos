522ef3f5bfd80d1ed828587eaf4f2e6b062edc22
veronica989 <veroca989@gmail.com>
2021-11-19 13:12:27 -0500

    First springboot setup

---
+++ src/main/java/com/misiontic/vehiculo_ms/VehiculoMsApplication.java
@@ -0,0 +1,13 @@
+package com.misiontic.vehiculo_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiculoMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiculoMsApplication.class, args);
+	}
+
+}
