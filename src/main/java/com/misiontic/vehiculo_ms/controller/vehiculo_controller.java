5936d92f686675c93754d7ccd7e9b4ee9c9cea48
veronica989 <veroca989@gmail.com>
2021-11-26 21:16:40 -0500

    PG 33 documentation in swagger

--- src/main/java/com/misiontic/vehiculo_ms/controller/vehiculo_controller.java
+++ src/main/java/com/misiontic/vehiculo_ms/controller/vehiculo_controller.java
@@ -2,7 +2,15 @@ package com.misiontic.vehiculo_ms.controller;
 import com.misiontic.vehiculo_ms.exceptions.ExepcionNotFound;
 import com.misiontic.vehiculo_ms.models.vehiculo;
 import com.misiontic.vehiculo_ms.repository.vehiculo_repository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
+
+import java.awt.print.Book;
 import java.util.Date;
 import java.util.List;
 
@@ -23,11 +31,18 @@ public class vehiculo_controller {
         return "Welcome to vehiculoMS";
     }
 
-    @GetMapping("/vehiculo/{placa}")
-    vehiculo getvehiculo(@PathVariable String placa) {
-        return (vehiculo) Vehiculo_repository.findById(placa).orElseThrow(() -> new ExepcionNotFound("no se encontro vehiculo" + placa));
+    //@GetMapping("/vehiculo/{placa}")
+    //vehiculo getvehiculo(@PathVariable String placa) {
+        //return (vehiculo) Vehiculo_repository.findById(placa).orElseThrow(() -> new ExepcionNotFound("no se encontro vehiculo" + placa));
 
-    }
+    //}
+    @Operation(summary = "Agregar un vehiculo")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Vehiculo agregrado",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class)) }),
+            @ApiResponse(responseCode = "400", description = "Datos incorrectos",
+                    content = @Content)})
 
     @PostMapping("/vehiculo")
     vehiculo newvehiculo(@RequestBody vehiculo Vehiculo) {
@@ -35,6 +50,14 @@ public class vehiculo_controller {
         return (vehiculo) Vehiculo_repository.save(Vehiculo);
 
     }
+
+    @Operation(summary = "Eliminar vehiculo por placa")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Vehiculo eliminado con exito",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class)) }),
+            @ApiResponse(responseCode = "404", description = "Vehiculo no encontrado",
+                    content = @Content)})
 
     @DeleteMapping("/vehiculo/delete/{placa}")
     String deleteVehiculo(@PathVariable String placa) {
@@ -46,6 +69,14 @@ public class vehiculo_controller {
         return "eliminacion ejecutada exitosamente";
     }
 
+    @Operation(summary = "Actualizar vehiculo por placa")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Vehiculo actualizado con exito",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class)) }),
+            @ApiResponse(responseCode = "404", description = "Vehiculo no encontrado",
+                    content = @Content)})
+
     @PutMapping("/vehiculo/update")
     vehiculo updatevehiculo(@RequestBody vehiculo Vehiculoupd) {
         vehiculo Vehiculo = Vehiculo_repository.findById(Vehiculoupd.getplaca()).orElse(null);
@@ -55,7 +86,13 @@ public class vehiculo_controller {
         return Vehiculo_repository.save(Vehiculo);
 
     }
-
+    @Operation(summary = "Consultar vehiculo por placa")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Vehiculo encontrado con exito",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class)) }),
+            @ApiResponse(responseCode = "404", description = "Vehiculo no encontrado",
+                    content = @Content)})
     @GetMapping("/vehiculo/all/{placa}")
     vehiculo getonevehiculo(@PathVariable String placa) {
         vehiculo Vehiculo = Vehiculo_repository.findById(placa).orElse(null);
